4556f8683845882888b5e7ecf68104aeffc5d49d
Arnab Biswas <83111868+ArnabbiswasAB@users.noreply.github.com>
2022-09-06 19:17:57 +0530

    Time: 100 ms (14.86%), Space: 30 MB (31.83%) - LeetHub

--- 115-distinct-subsequences/115-distinct-subsequences.cpp
+++ 115-distinct-subsequences/115-distinct-subsequences.cpp
@@ -9,9 +9,29 @@ public:
             ans += RecursionAndMemoization(s, i-1, t , j-1, dp);
         return dp[i][j] = ans;
     }
-    int numDistinct(string s, string t) {
-        int n = s.size(), m = t.size();
-        vector<vector<int>> dp(n+1, vector<int>(m+1, -1));
-        return RecursionAndMemoization(s, n, t, m, dp);
+    int numDistinct(string s1, string s2) {
+        int n = s1.size(), m = s2.size();
+        
+        vector<vector<double>> dp(n+1,vector<double>(m+1,0));
+    
+    for(int i=0;i<n+1;i++){
+        dp[i][0]=1;
+    }
+    for(int i=1;i<m+1;i++){
+        dp[0][i]=0;
+    }
+    
+    for(int i=1;i<n+1;i++){
+        for(int j=1;j<m+1;j++){
+            
+            if(s1[i-1]==s2[j-1])
+                dp[i][j] = (dp[i-1][j-1] + dp[i-1][j]);
+            else
+                dp[i][j] = dp[i-1][j];
+        }
+    }
+    
+    
+    return (int)dp[n][m];
     }
 };
