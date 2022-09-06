cfc8ec97a1c749c5dc52dbe5c61b0f265577a190
Arnab Biswas <83111868+ArnabbiswasAB@users.noreply.github.com>
2022-09-06 22:32:09 +0530

    Time: 27 ms (64.52%), Space: 11.8 MB (71.55%) - LeetHub

--- 518-coin-change-2/518-coin-change-2.cpp
+++ 518-coin-change-2/518-coin-change-2.cpp
@@ -1,5 +1,7 @@
 class Solution {
 public:
+    
+    /*
    int countWaysToMakeChangeUtil(vector<int>& arr,int ind, int amt, vector<vector<int>>& dp){
        
     if(ind == 0){
@@ -17,6 +19,7 @@ public:
         
     return dp[ind][amt] = notTaken + taken;
 }
+   
     
     
     int change(int amount, vector<int>& coins) {
@@ -24,5 +27,28 @@ public:
         int n = coins.size();
          vector<vector<int>> dp(n,vector<int>(amount+1,-1));
     return countWaysToMakeChangeUtil(coins,n-1, amount, dp);
+    }
+    
+    */
+     int change(int amount, vector<int>& coins) {
+        
+        int n = coins.size();
+         int dp[n+1][amount+1];
+         
+         for(int i=0; i<n+1; i++){
+             for(int j=0; j<amount+1; j++){
+                 
+                 if(i == 0)
+                     dp[i][j] = 0;
+                 else if(j == 0)
+                     dp[i][j] = 1;
+                 else if(j < coins[i-1])
+                     dp[i][j] = dp[i-1][j];
+                 else
+                     dp[i][j] = dp[i-1][j] + dp[i][j - coins[i-1]];
+             }
+         }
+         
+         return dp[n][amount];
     }
 };
