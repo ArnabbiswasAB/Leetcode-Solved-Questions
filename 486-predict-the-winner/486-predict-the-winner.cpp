d1ad0c6b4f53962692300ec0cd5f532dcc2558af
Arnab Biswas <83111868+ArnabbiswasAB@users.noreply.github.com>
2022-09-08 22:21:58 +0530

    Time: 4 ms (65.53%), Space: 7.7 MB (34.07%) - LeetHub

---
+++ 486-predict-the-winner/486-predict-the-winner.cpp
@@ -0,0 +1,29 @@
+class Solution {
+public:
+    int solve(vector<int>& nums , int i, int e , vector<vector<int>> &dp){
+     
+        if(i>e) 
+            return 0;
+        
+        if(dp[i][e] != -1)
+            return dp[i][e];
+        
+        int op1 = nums[i] + min(solve(nums,i+2,e,dp), solve(nums,i+1,e-1,dp));
+        
+        int op2 = nums[e] + min(solve(nums,i,e-2,dp), solve(nums,i+1,e-1,dp));
+        
+        return dp[i][e] = max(op1,op2);
+    }
+    
+    
+    bool PredictTheWinner(vector<int>& nums) {
+         // if both player will play optimally then
+        // palyer 1 will pick best optimall index and then player 2 will choose optimal index too to win
+        // let us find total sum and then find if player 1 play optimally then his score and then see .
+        int tsum = 0;
+        for(int x: nums) tsum += x;
+        vector<vector<int>> dp(nums.size() , vector<int> (nums.size(), -1));
+        int s = solve(nums,0 , nums.size()-1 , dp);
+        return dp[0][nums.size()-1] >= tsum-dp[0][nums.size()-1] ? 1:0;
+    }
+};
