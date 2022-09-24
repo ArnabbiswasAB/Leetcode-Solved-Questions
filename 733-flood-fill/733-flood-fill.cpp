5a84fbcd34c15d3986ceaa96b5f2e81aeb7dd5e1
Arnab Biswas <83111868+ArnabbiswasAB@users.noreply.github.com>
2022-09-24 10:43:34 +0530

    Time: 22 ms (23.19%), Space: 14.6 MB (8.31%) - LeetHub

--- 733-flood-fill/733-flood-fill.cpp
+++ 733-flood-fill/733-flood-fill.cpp
@@ -22,7 +22,34 @@ public:
         int n = image[0].size();
         
         int old = image[sr][sc];
-        dfs(sr, sc, m, n, image, old , color);
+       // dfs(sr, sc, m, n, image, old , color);
+        vector<vector<int>>visited(m,vector<int>(n,0));
+        
+       // memset(visited, false, sizeof(visited));
+        
+       queue<pair<int,int>>q;
+        q.push({sr,sc});
+        
+        int dx[4] = {1,-1,0,0};
+        int dy[4] = {0,0,1,-1};
+        
+        while(!q.empty()){
+            
+            auto it = q.front();
+            q.pop();
+             image[it.first][it.second] = color;
+            for(int i=0; i<4; i++){
+                int x = it.first + dx[i];
+                int y = it.second + dy[i];
+                
+                if(x>=0 && y>=0 && x<m && y<n && image[x][y] == old && !visited[x][y]){
+                   
+                    q.push({x,y});
+                    visited[x][y] = 1;
+                }
+            }
+        }
+        
         return image;
     }
 };
