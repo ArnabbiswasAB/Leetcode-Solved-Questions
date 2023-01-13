90b03458396ce90ff06f9e9d2b64b393a210e7bd
Arnab Biswas <83111868+ArnabbiswasAB@users.noreply.github.com>
2023-01-13 11:09:52 +0530

    Added solution - LeetHub

--- Connecting the graph - GFG/connecting-the-graph.cpp
+++ Connecting the graph - GFG/connecting-the-graph.cpp
@@ -4,67 +4,58 @@ using namespace std;
 
 
 // } Driver Code Ends
-//User function Template for C++
+// User function Template for C++
 
 
-class disjointSet{
+class DisjointSet{
     public:
-    
-    vector<int> size,parent,rank;
-    
-    disjointSet(int n){
-        size.resize(n+1);
-        parent.resize(n+1);
+    vector<int>rank,parent,size;
+    DisjointSet(int n){
         rank.resize(n+1,0);
-        
-        for(int i=0; i<=n; i++){
+        size.resize(n+1,1);
+        parent.resize(n+1);
+        for(int i=0; i<n; i++){
             parent[i] = i;
-            size[i] = 1;
-         }
+        }
     }
-    
-    int findParent(int node){
-        
+
+    int findPar(int node){
         if(node == parent[node])
         return node;
-        
-        return parent[node] = findParent(parent[node]);
+
+        return parent[node] = findPar(parent[node]);
     }
-    
-    
-    void UnionByRank(int u, int v){
-        
-        int u_p = findParent(u);
-        int v_p = findParent(v);
-        
+
+    void UnionRank(int u, int v){
+        int u_p = findPar(u);
+        int v_p = findPar(v);
+
         if(u_p == v_p)
-        return;
-        
+        return ;
+
         if(rank[u_p] < rank[v_p]){
             parent[u_p] = v_p;
         }
         else if(rank[v_p] < rank[u_p]){
             parent[v_p] = u_p;
-        }
-        else
-        {
+        }else{
             parent[v_p] = u_p;
             rank[u_p]++;
         }
     }
-    
-    
-    void UnionBySize(int u, int v){
-        
-        int u_p = findParent(u);
-        int v_p = findParent(v);
-        
-        if(size[u_p] < size[v_p])
-        {
+
+
+    void UnionSize(int u, int v){
+        int u_p = findPar(u);
+        int v_p = findPar(v);
+
+        if(u_p == v_p)
+        return;
+
+        if(size[u_p] < size[v_p]){
             parent[u_p] = v_p;
             size[v_p] += size[u_p];
-        }
-        else{
+        }else{
             parent[v_p] = u_p;
             size[u_p] += size[v_p];
         }
@@ -72,37 +63,38 @@ class disjointSet{
 };
 
 
+
 class Solution {
   public:
-    int Solve(int n, vector<vector<int>>& edge){
+    int Solve(int n, vector<vector<int>>& edges) {
         // code here
         
-        disjointSet ds(n);
-        int cnt = 0;
-        
-        for(auto it : edge){
-            int u = it[0];
-            int v = it[1];
-            
-            if(ds.findParent(u) == ds.findParent(v)){
-                cnt++;
-            }
-            else{
-                ds.UnionBySize(u,v);
-            }
-        }
-        
-        int countParent = 0;
-        for(int i=0; i<n; i++){
-            if(ds.parent[i] == i)
-            countParent++;
-        }
-        
-        int ans = countParent - 1;
-        if(cnt >= ans)
-        return ans;
-        
-        return -1;
+         DisjointSet ds(n);
+          int cntExtras = 0;
+
+          for(auto it : edges){
+              int u = it[0];
+              int v = it[1];
+
+              if(ds.findPar(u) == ds.findPar(v)){
+                  cntExtras++;
+              }else{
+                  ds.UnionSize(u,v);
+              }
+          }
+
+          int cnt = 0;
+          for(int i=0; i<n; i++){
+              if(i == ds.findPar(i))
+              cnt++;
+          }
+
+          int ans = cnt - 1;
+          if(cntExtras >= ans)
+            return ans;
+
+          return -1;  
+          
     }
 };
 
@@ -112,20 +104,22 @@ int main() {
     int t;
     cin >> t;
     while (t--) {
-        int n,m; cin>>n>>m;
-        vector<vector<int>>adj;
-        
-        for(int i=0; i<m; ++i){
+        int n, m;
+        cin >> n >> m;
+        vector<vector<int>> adj;
+
+        for (int i = 0; i < m; ++i) {
             vector<int> temp;
-            for(int j=0; j<2; ++j){
-                int x; cin>>x;
+            for (int j = 0; j < 2; ++j) {
+                int x;
+                cin >> x;
                 temp.push_back(x);
             }
             adj.push_back(temp);
         }
-            
-            Solution Obj;
-            cout<<Obj.Solve(n,adj)<<"\n";
+
+        Solution Obj;
+        cout << Obj.Solve(n, adj) << "\n";
     }
     return 0;
 }
