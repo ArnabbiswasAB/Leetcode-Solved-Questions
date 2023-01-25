d7d077a07f9071b7e8fdb2220efcd562b57634bc
Arnab Biswas <83111868+ArnabbiswasAB@users.noreply.github.com>
2023-01-25 22:22:44 +0530

    Added solution - LeetHub

--- Find All Four Sum Numbers - GFG/find-all-four-sum-numbers.cpp
+++ Find All Four Sum Numbers - GFG/find-all-four-sum-numbers.cpp
@@ -10,47 +10,46 @@ class Solution{
     public:
     // arr[] : int input array of integers
     // k : the quadruple sum required
-    vector<vector<int> > fourSum(vector<int> &arr, int k) {
+    vector<vector<int> > fourSum(vector<int> &arr, int target) {
         // Your code goes here
-        
-        vector<vector<int>>ans;
-        int n = arr.size();
-        sort(arr.begin(),arr.end());
-        
-        for(int i=0; i<n; i++){
-            for(int j=i+1; j<n; j++){
-                
-                int sum = k - arr[i] - arr[j];
-                int k = j+1,l=n-1;
-                
-                while(k<l){
-                    if(arr[k] + arr[l] == sum){
-                        ans.push_back({arr[i],arr[j],arr[k],arr[l]});
-                        
-                        while(k<l && arr[k] == arr[k+1])
-                        k++;
-                        
-                        while(k<l && arr[l] == arr[l-1])
-                        l--;
-                        
-                        k++; l--;
-                    }
-                    else if(arr[k] + arr[l] < sum){
-                        k++;
-                    }else{
-                        l--;
-                    }
-                }
-                
-                while(j+1<n && arr[j] == arr[j+1])
-                j++;
-            }
-            
-            while(i+1<n && arr[i] == arr[i+1])
-            i++;
-        }
-        
-        return ans;
+         vector<vector<int>> ans;
+         sort(arr.begin() , arr.end());
+         int n = arr.size();
+         
+         for(int i=0; i<n; i++){
+             for(int j=i+1; j<n; j++){
+                 
+                  int k = j+1, l=n-1;
+                  while(k<l){
+                      
+                      if(arr[i] + arr[j] + arr[k] + arr[l] == target){
+                          ans.push_back({arr[i],arr[j],arr[k],arr[l]});
+                          
+                          while(k<l && arr[k] == arr[k+1])
+                          k++;
+                          
+                          while(k<l && arr[l] == arr[l-1])
+                          l--;
+                          
+                          k++;
+                          l--;
+                      }
+                      else if(arr[i] + arr[j] + arr[k] + arr[l] < target)
+                      k++;
+                      else
+                      l--;
+                  }
+                  
+                  while(j+1<n && arr[j] == arr[j+1])
+                  j++;
+             }
+             
+             while(i+1<n && arr[i] == arr[i+1])
+             i++;
+         }
+         
+         
+         return ans;
     }
 };
 
