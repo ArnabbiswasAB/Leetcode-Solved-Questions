cb074915fd6ba1b83b5a4327727af058d6d3c0ce
Arnab Biswas <83111868+ArnabbiswasAB@users.noreply.github.com>
2023-02-02 20:26:47 +0530

    Added solution - LeetHub

--- Partitions with Given Difference - GFG/partitions-with-given-difference.cpp
+++ Partitions with Given Difference - GFG/partitions-with-given-difference.cpp
@@ -32,11 +32,11 @@ class Solution {
     
     
     
-    int countPartitions(int n, int diff, vector<int>& arr) {
+    int countPartitions(int n, int diff, vector<int>& num) {
         
         long long sum = 0;
         for(int i=0; i<n; i++){
-            sum += arr[i];
+            sum += num[i];
         }
         
        if(sum - diff < 0)
@@ -45,10 +45,33 @@ class Solution {
        if((sum-diff)%2 == 1)
        return 0;
        
-       int target = (sum - diff)/2;
+       int tar = (sum - diff)/2;
         
-        vector<vector<int>>dp(n, vector<int>(target+1, -1));
-        return f(n-1, target, arr, dp);
+        vector<vector<int>>dp(n, vector<int>(tar+1, 0));
+       // return f(n-1, target, arr, dp);
+       
+       
+        if(num[0] == 0)
+        dp[0][0] =2;  // 2 cases -pick and not pick
+        else
+        dp[0][0] = 1;  // 1 case - not pick
+    
+    if(num[0]!=0 && num[0]<=tar) 
+    dp[0][num[0]] = 1;  // 1 case -pick
+    
+    for(int ind = 1; ind<n; ind++){
+        for(int target= 0; target<=tar; target++){
+            
+            int notTaken = dp[ind-1][target];
+    
+            int taken = 0;
+                if(num[ind]<=target)
+                    taken = dp[ind-1][target-num[ind]];
+        
+            dp[ind][target]= (notTaken + taken)%mod;
+        }
+    }
+    return dp[n-1][tar];
     }
 };
 
