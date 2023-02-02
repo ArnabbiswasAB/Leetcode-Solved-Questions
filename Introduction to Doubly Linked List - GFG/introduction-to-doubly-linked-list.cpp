e30aa616d62b6e8546ff413a8da36855f69e1725
Arnab Biswas <83111868+ArnabbiswasAB@users.noreply.github.com>
2023-02-02 20:38:39 +0530

    Added solution - LeetHub

---
+++ Introduction to Doubly Linked List - GFG/introduction-to-doubly-linked-list.cpp
@@ -0,0 +1,102 @@
+//{ Driver Code Starts
+// Initial Template for C++
+
+#include <bits/stdc++.h>
+using namespace std;
+
+class Node{
+public:
+    Node* prev;
+    int data;
+    Node* next;
+ 
+    Node()
+    { 
+        prev = NULL; 
+        data = 0;
+        next = NULL;
+    }
+    
+    Node(int value)
+    { 
+        prev = NULL; 
+        data = value;
+        next = NULL;
+    }
+};
+
+
+// } Driver Code Ends
+// User function Template for C++
+
+/*
+class Node{
+public:
+    Node* prev;
+    int data;
+    Node* next;
+ 
+    Node()
+    { 
+        prev = NULL; 
+        data = 0;
+        next = NULL;
+    }
+    
+    Node(int value)
+    { 
+        prev = NULL; 
+        data = value;
+        next = NULL;
+    }
+};*/
+
+class Solution {
+  public:
+    Node* constructDLL(vector<int>& arr) {
+        // code here
+        
+        int i=0, n = arr.size();
+        Node* prev = NULL, *head = NULL;
+        
+        for(int i=0; i<n; i++){
+            
+            Node* new_node = new Node(arr[i]);
+            
+            if(head == NULL)
+            head = new_node;
+            
+            new_node->next = NULL;
+            new_node->prev = prev;
+            if(prev != NULL)
+              prev->next = new_node;
+              
+            prev = new_node;
+        }
+        
+        return head;
+    }
+};
+
+//{ Driver Code Starts.
+
+int main() {
+    int t;
+    cin >> t;
+    while (t--) {
+        int n;
+        cin >> n;
+        vector<int> arr(n);
+        for (int i = 0; i < n; i++) cin >> arr[i];
+        Solution obj;
+        Node* ans = obj.constructDLL(arr);
+        while (ans) {
+            cout << ans->data << " ";
+            ans = ans->next;
+        }
+        cout << "\n";
+    }
+    return 0;
+}
+
+// } Driver Code Ends
