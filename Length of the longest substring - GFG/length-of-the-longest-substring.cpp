991c72c0bdbc298bf04aedff3a6e9ee53ab14352
Arnab Biswas <83111868+ArnabbiswasAB@users.noreply.github.com>
2023-02-11 18:36:52 +0530

    Added solution - LeetHub

--- Length of the longest substring - GFG/length-of-the-longest-substring.cpp
+++ Length of the longest substring - GFG/length-of-the-longest-substring.cpp
@@ -7,42 +7,44 @@ using namespace std;
 class Solution{
     public:
     int longestUniqueSubsttr(string s){
-    
-        vector<int>uni(256,-1); //string can have only 256 diff characters at a time
-       int i=0,j=0,n=s.size();
+        //code
         
-        if(n==0) return 0;  // 0 characters present
+      /*  unordered_map<char,int>mp;
+        int ans = 0, i = 0, j = 0, n = s.length();
         
-        int len=0,maxlen=0;
-        while(j<n ){
+        while(i<n){
             
-            if(uni[s[j]]!=-1){          //if element is present already
-                i=max(uni[s[j]]+1,i);
-                
-            }
-            uni[s[j]]=j;                //if element is not present
-            len=j-i+1;
-            maxlen=max(len,maxlen);
-            j++;                //moving right
-            
-            
+            if(mp.find(s[i]) != mp.end())
+              j = max(mp[s[i]]+1,);
+              
+              
+              mp[s[i]] = i;
+              ans = max(ans, i - j + 1);
+              i++;
         }
         
-        return maxlen;
-        /*
-        int n = s.length();
-		int j=0,mx=0;
-		unordered_map<char,int> mp;
-		for(int i=0;i<n;i++) {
-			mp[s[i]]++;
-			while(mp[s[i]]>1) {
-				mp[s[j]]--;
-				j++;
-			}
-			mx=max(mx,i-j+1);
-		}
-		return mx;
+        return ans;
         */
+        
+        vector<int>mp(256,0);
+        int ans = 0, i = 0, j = 0, n = s.length();
+        
+        
+        while(i<n){
+           
+           mp[s[i] - 'a']++;
+           
+           while(mp[s[i] - 'a'] > 1){
+               mp[s[j] - 'a']--;
+               j++;
+           }
+           
+           ans = max(ans, i - j + 1);
+           i++;
+        }
+        
+        
+        return ans;
     }
 };
 
