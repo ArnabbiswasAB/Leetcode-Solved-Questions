866a2016f6ad936807e132432455c3e4908008bf
Arnab Biswas <83111868+ArnabbiswasAB@users.noreply.github.com>
2023-02-15 22:17:18 +0530

    Added solution - LeetHub

--- Maximum Connected group - GFG/maximum-connected-group.cpp
+++ Maximum Connected group - GFG/maximum-connected-group.cpp
@@ -7,136 +7,110 @@ using namespace std;
 
 // } Driver Code Ends
 // User function Template for C++
-class DisjointSet {
+class DisjointSet{
+    public: 
     
-    public :
-       vector<int>rank, parent, size;
-       
-       DisjointSet(int n){
-           rank.resize(n+1,0);
-           size.resize(n+1,1);
-           parent.resize(n+1);
-           for(int i=0; i<n; i++){
-               parent[i] = i;
-           }
-       }
-       
-       
-       int findPar(int node){
-           if(node == parent[node])
-           return node;
-           
-           return parent[node] = findPar(parent[node]);
-       }
-       
-       void unionByrank(int u, int v){
-           int u_p = findPar(u);
-           int v_p = findPar(v);
-           
-           if(u_p == v_p)
-           return;
-           
-           if(rank[u_p] < rank[v_p]){
-               parent[u_p] = v_p;
-           }else if(rank[v_p] < rank[u_p]){
-               parent[v_p] = u_p;
-           }else{
-               parent[u_p] = v_p;
-               rank[v_p]++;
-           }
-       }
-       
-       void unionBySize(int u, int v){
-           int u_p = findPar(u);
-           int v_p = findPar(v);
-           
-           if(u_p == v_p)
-           return;
-           
-           if(size[u_p] < size[v_p]){
-               parent[u_p] = v_p;
-               size[v_p] += size[u_p];
-           }else{
-               parent[v_p] = u_p;
-               size[u_p] += size[v_p];
-           }
-       }
-       
+    vector<int>par;
+    vector<int>size;
+
+    DisjointSet(int n){
+        par.resize(n+1);
+        size.resize(n+1,1);
+
+        for(int i=0;i<=n;i++){
+            par[i] = i;
+        }
+    }
+
+    int findpar(int u){
+        if(u == par[u]){
+            return u;
+        }
+
+        return par[u] = findpar(par[u]);
+    }
+
+    void unionSize(int u,int v){
+        u = findpar(u);
+        v = findpar(v);
+
+        if(u == v) return;
+
+        if(size[u] < size[v]){
+            par[u] = v;
+            size[v] += size[u];
+        }
+        else{
+            par[v] = u;
+            size[u] += size[v]; 
+        }
+    }
 };
 
-
-
 class Solution {
-   private:
-    bool isValid(int row,int col,int n,int m){
-        return  row>=0&&row<n&&col>=0&&col<m;
-    }
   public:
-  
     int MaxConnection(vector<vector<int>>& grid) {
+        // code here
         
-        int n=grid.size();
+        int n = grid.size();
         DisjointSet ds(n*n);
         
-        for(int row=0;row<n;row++){
-            for(int col=0;col<n;col++){
+        int dx[4] = {1,-1,0,0};
+        int dy[4] = {0,0,-1,1};
+        
+        for(int i=0; i<n; i++){
+            for(int j=0; j<n; j++){
                 
-                if(grid[row][col]==0)continue;
-                int dr[]={-1,0,1,0};
-                int dc[]={0,1,0,-1};
-                for(int idx=0;idx<4;idx++){
+                if(grid[i][j] == 0)
+                continue;
+                
+                for(int ind = 0; ind<4; ind++){
                     
-                    int nrow=row+dr[idx];
-                    int ncol=col+dc[idx];
+                    int row = i + dx[ind];
+                    int col = j + dy[ind];
                     
-                    if(isValid(nrow,ncol,n,n)&&grid[nrow][ncol]==1){
+                    if(row>=0 && col>=0 && row<n && col<n && grid[row][col] == 1){
+                        int nodeNo = i*n + j;
+                        int adjNo = row*n + col;
                         
-                        int nodeNo=n*row+col;
-                        int adjNodeNO=n*nrow+ncol;
-                        ds.unionBySize(nodeNo,adjNodeNO);
-                        
+                        ds.unionSize(nodeNo, adjNo);
                     }
-                    
                 }
-                
-            }
-        }
-        int mx=0;
-         for(int row=0;row<n;row++){
-            for(int col=0;col<n;col++){
-                
-                if(grid[row][col]==1)continue;
-                int dr[]={-1,0,1,0};
-                int dc[]={0,1,0,-1};
-                set<int>components;
-                for(int idx=0;idx<4;idx++){
-                    
-                    int nrow=row+dr[idx];
-                    int ncol=col+dc[idx];
-                    
-                    if(isValid(nrow,ncol,n,n)&&grid[nrow][ncol]==1){
-                        
-                        int nodeNo=n*row+col;
-                        int adjNodeNO=n*nrow+ncol;
-                        components.insert(ds.findPar(nrow*n+ncol));
-                        
-                    }
-                    
-                }
-                int totalSize=0;
-                for(auto it:components){
-                    totalSize+=ds.size[it];
-                }
-                
-                mx=max(mx,totalSize+1);
-                
             }
         }
         
-       // for(int cell=0;cell<n*n;cell++){
-            mx=max(mx,ds.size[ds.findPar(0)]);
-      //  }
-        return mx;
+        int maxi = 0;
+       
+        
+        for(int i=0; i<n; i++){
+            for(int j=0; j<n; j++){
+                
+                if(grid[i][j] == 1)
+                continue;
+                
+                 set<int>st;
+                 
+                 for(int ind=0; ind<4; ind++){
+                     int row = i + dx[ind];
+                     int col = j + dy[ind];
+                     
+                     if(row>=0 && col>=0 && row<n && col<n && grid[row][col] == 1){
+                         st.insert(ds.findpar(row*n + col));
+                     }
+                 }
+                 
+                 int total = 0;
+                 
+                 for(auto it : st){
+                     total += ds.size[it];
+                 }
+                 
+                 maxi = max(maxi, total + 1);
+            }
+        }
+        
+       maxi = max(maxi, ds.size[ds.findpar(0)]); 
+        return maxi;
     }
 };
 
