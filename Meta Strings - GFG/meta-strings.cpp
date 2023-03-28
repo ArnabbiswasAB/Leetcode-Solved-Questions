13fa1f552a1f9330b774a10be7f2244549f86dd0
Arnab Biswas <83111868+ArnabbiswasAB@users.noreply.github.com>
2023-03-28 22:29:23 +0530

    Added solution - LeetHub

---
+++ Meta Strings - GFG/meta-strings.cpp
@@ -0,0 +1,49 @@
+//{ Driver Code Starts
+#include<bits/stdc++.h>
+using namespace std;
+bool metaStrings (string s1, string s2);
+int main()
+{
+    int t; cin >> t;
+    while (t--)
+    {
+        string s1; cin >> s1;
+        string s2; cin >> s2;
+
+        cout << metaStrings (s1, s2) << endl;
+    }
+}
+// Contributed By: Pranay Bansal
+
+// } Driver Code Ends
+
+
+bool metaStrings (string s1, string s2)
+{
+    // your code here
+    
+    int i=0, j=0, n = s1.length(), m = s2.length();
+    
+    if(n != m || s1 == s2)
+    return false;
+    
+   int first = -1, second = -1;
+   for(i=0; i<n; i++){
+       
+       if(s1[i] == s2[i]){
+           continue;
+       }
+       else{
+           
+           if(first == -1)
+            first = i;
+           else
+           break;
+       }
+   }
+   
+   if(i != n)
+   swap(s1[i],s1[first]);
+   
+   return s1 == s2;
+}
