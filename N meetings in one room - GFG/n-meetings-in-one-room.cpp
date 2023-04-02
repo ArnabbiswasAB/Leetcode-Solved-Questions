923750534e4dab707aee7770c98f374b652dd357
Arnab Biswas <83111868+ArnabbiswasAB@users.noreply.github.com>
2023-04-02 22:57:29 +0530

    Added solution - LeetHub

--- N meetings in one room - GFG/n-meetings-in-one-room.cpp
+++ N meetings in one room - GFG/n-meetings-in-one-room.cpp
@@ -3,13 +3,12 @@
 using namespace std;
 
 // } Driver Code Ends
-
 class Solution
 {
     public:
     //Function to find the maximum number of meetings that can
     //be performed in a meeting room.
-    static bool cmp(pair<int,int>a, pair<int,int>b){
+   static bool cmp(pair<int,int>a, pair<int,int>b){
        
        if(a.second == b.second)
        return a.first < b.first;
@@ -37,6 +36,7 @@ class Solution
                 count++;
                 prev = curr[i].second;
             }
+            
         }
         
         return count;
