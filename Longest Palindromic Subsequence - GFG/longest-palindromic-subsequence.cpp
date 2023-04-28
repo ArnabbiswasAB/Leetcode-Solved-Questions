a1f9bab0f78d4fe287429878244fcc45053bd554
Arnab Biswas <83111868+ArnabbiswasAB@users.noreply.github.com>
2023-04-28 20:40:15 +0530

    Added solution - LeetHub

--- Longest Palindromic Subsequence - GFG/longest-palindromic-subsequence.cpp
+++ Longest Palindromic Subsequence - GFG/longest-palindromic-subsequence.cpp
@@ -10,30 +10,28 @@ using namespace std;
 
 class Solution{
   public:
-  
-  int helper(string A,string B,int m,int n,vector<vector<int>>& dp){
-   
-   if(m < 0 || n < 0)
-   return 0;
-   
-   if(dp[m][n] != -1)
-   return dp[m][n];
-   
-   if(A[m] == B[n])
-      return dp[m][n] = 1 + helper(A,B, m-1, n-1, dp);
-      
-   
-   return dp[m][n] = max(helper(A,B,m-1,n,dp), helper(A,B,m,n-1,dp));      
-  }
-  
-  
+    int helper(string A, int i, string temp, int j, vector<vector<int>> &dp){
+        
+        if(i<0 || j<0)
+        return 0;
+        
+        if(dp[i][j] != -1)
+        return dp[i][j];
+        
+        if(A[i] == temp[j])
+          return dp[i][j] = helper(A,i-1,temp,j-1,dp) + 1;
+        
+        return dp[i][j] = max(helper(A,i-1,temp,j,dp), helper(A,i,temp,j-1,dp));  
+    }
+    
     int longestPalinSubseq(string A) {
-        //code here
-        string B = A;
-        reverse(B.begin(), B.end());
-        int n = A.size();
+       
+        int n = A.length();
         vector<vector<int>> dp(n, vector<int>(n,-1));
-        return helper(A,B,n-1,n-1,dp);
+        string temp = A;
+        reverse(temp.begin(),temp.end());
+        
+        return helper(A,n-1, temp, n-1, dp);
     }
 };
 
