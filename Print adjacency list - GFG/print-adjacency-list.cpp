55983c24f1d0a5c13149dd94393fe49ec4b094e7
Arnab Biswas <83111868+ArnabbiswasAB@users.noreply.github.com>
2023-06-22 19:39:24 +0530

    Added solution - LeetHub

--- Print adjacency list - GFG/print-adjacency-list.cpp
+++ Print adjacency list - GFG/print-adjacency-list.cpp
@@ -9,16 +9,17 @@ class Solution {
     vector<vector<int>> printGraph(int V, vector<int> adj[]) {
         // Code here
         
-         vector<vector<int>>v;
-         for(int i=0;i<V;i++){
-             vector<int>t;
-             t.push_back(i);
-             for(auto it:adj[i]){
-                 t.push_back(it);
+         vector<vector<int>> ans(V);
+         
+         for(int i=0; i<V; i++){
+             ans[i].push_back(i);
+             for(auto it : adj[i]){
+                 
+                 ans[i].push_back(it);
              }
-             v.push_back(t);
          }
-         return v;
+         
+         return ans;
     }
 };
 
