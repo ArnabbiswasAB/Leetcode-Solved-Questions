b66b8324078f03da8c5cbfc32c9fca6dd7b55dbd
Arnab Biswas <83111868+ArnabbiswasAB@users.noreply.github.com>
2023-06-29 13:45:02 +0530

    Added solution - LeetHub

--- Mirror Tree - GFG/mirror-tree.cpp
+++ Mirror Tree - GFG/mirror-tree.cpp
@@ -98,14 +98,14 @@ class Solution {
   public:
     // Function to convert a binary tree into its mirror tree.
     void mirror(Node* node) {
-      
-      if(!node)
-      return;
-      
-      swap(node->left, node->right);
-      
-      mirror(node->left);
-      mirror(node->right);
+        // code here
+        if(!node)
+        return;
+        
+        swap(node->left, node->right);
+        
+        mirror(node->left);
+        mirror(node->right);
     }
 };
 
