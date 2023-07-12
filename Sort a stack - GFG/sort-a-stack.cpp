2bbcf53d0368079356aa70e7a646c27bcda48d72
Arnab Biswas <83111868+ArnabbiswasAB@users.noreply.github.com>
2023-07-12 18:59:17 +0530

    Added solution - LeetHub

--- Sort a stack - GFG/sort-a-stack.cpp
+++ Sort a stack - GFG/sort-a-stack.cpp
@@ -40,17 +40,6 @@ while(t--)
 // } Driver Code Ends
 
 
-/*The structure of the class is
-class SortedStack{
-public:
-	stack<int> s;
-	void sort();
-};
-*/
-
-/* The below method sorts the stack s 
-you are required to complete the below method */
-
 void sortedInsert(stack<int>& st, int x){
     
     if(st.empty() || st.top() < x){
