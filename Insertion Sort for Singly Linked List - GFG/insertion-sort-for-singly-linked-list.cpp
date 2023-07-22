60a21fbe9bdcbc90630f995eb66bc1547493de2a
Arnab Biswas <83111868+ArnabbiswasAB@users.noreply.github.com>
2023-07-22 19:29:20 +0530

    Added solution - LeetHub

--- Insertion Sort for Singly Linked List - GFG/insertion-sort-for-singly-linked-list.cpp
+++ Insertion Sort for Singly Linked List - GFG/insertion-sort-for-singly-linked-list.cpp
@@ -29,43 +29,43 @@ void printList(struct Node *head)
 
 
 // } Driver Code Ends
-
-
+//User function Template for C++
 
 class Solution
 {
     public:
     Node* insertionSort(struct Node* head)
     {
-      
-      if(!head || !head->next)
-      return head;
-      
-      Node* dummy = new Node(0);
-      dummy->next = head;
-      Node* outer = head;
-      
-      while(outer && outer->next){
-          
-          Node* inner = dummy, *temp = NULL;
-          while(inner != outer){
-              
-              if(inner->next->data > outer->next->data){
-                  temp = inner->next;
-                  inner->next = outer->next;
-                  outer->next = outer->next->next;
-                  inner->next->next = temp;
-                  break;
-              }
-              
-              inner = inner->next;
-          }
-          
-          if(inner == outer)
+        if(!head || !head->next)
+        return head;
+        
+        Node* dummy = new Node(0);
+        dummy->next = head;
+        
+        Node* outer = head;
+        
+        while(outer && outer->next){
+            
+            Node* inner = dummy, *prev = NULL;
+            
+            while(inner != outer){
+                
+                if(inner->next->data > outer->next->data){
+                    prev = inner->next;
+                    inner->next = outer->next;
+                    outer->next = outer->next->next;
+                    inner->next->next = prev;
+                    break;
+                }
+                
+                inner = inner->next;
+            }
+            
+            if(inner == outer)
             outer = outer->next;
-      }
-      
-      return dummy->next;
+        }
+        
+        return dummy->next;
     }
 };
 
