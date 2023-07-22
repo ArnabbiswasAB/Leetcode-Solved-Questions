5d4de7e2ffd5ad03e2ede680c2971e2fffb13970
Arnab Biswas <83111868+ArnabbiswasAB@users.noreply.github.com>
2023-07-23 00:39:17 +0530

    Added solution - LeetHub

--- Remove Half Nodes - GFG/remove-half-nodes.cpp
+++ Remove Half Nodes - GFG/remove-half-nodes.cpp
@@ -104,22 +104,20 @@ int main()
 }
 // } Driver Code Ends
 
-// Return the root of the modified tree after removing all the half nodes.
+
+
 Node *RemoveHalfNodes(Node *root)
 {
-   //add code here.
-   if(!root)
-   return NULL;
+   if(!root || (!root->left && !root->right))
+   return root;
    
    root->left = RemoveHalfNodes(root->left);
    root->right = RemoveHalfNodes(root->right);
    
-   if (root->left==NULL && root->right==NULL) 
-    return root; 
-        
-   if(!root->left)
+   if(!root->left && root->right)
    return root->right;
-   else if(!root->right)
+   
+   if(root->left && !root->right)
    return root->left;
    
    return root;
