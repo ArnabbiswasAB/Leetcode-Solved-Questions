8b5a9d23d2656c506efa7250dae566108fb88c48
Arnab Biswas <83111868+ArnabbiswasAB@users.noreply.github.com>
2023-07-26 00:02:54 +0530

    Added solution - LeetHub

--- K-th element of two sorted Arrays - GFG/kth-element-of-two-sorted-arrays.cpp
+++ K-th element of two sorted Arrays - GFG/kth-element-of-two-sorted-arrays.cpp
@@ -8,38 +8,32 @@ class Solution{
     public:
     int kthElement(int arr1[], int arr2[], int n, int m, int k)
     {
-    
-    if(n>m){
-    return kthElement(arr2,arr1,m,n,k);
-    }
-    
-    // Apply BS on smaller array    
-        // K is larger than arr2 length
-    int start=max(0,k-m);
-    // k is smaller than arr1 length
-    int end=min(k,n);
-    
-    while(start<=end){
-        int cut1=start +(end-start)/2;
-        int cut2=k-cut1;
-        int l1=cut1!=0?arr1[cut1-1]:INT_MIN;
-        int l2=cut2!=0?arr2[cut2-1]:INT_MIN;
-        int r1=cut1!=n?arr1[cut1]:INT_MAX;
-        int r2=cut2!=m?arr2[cut2]:INT_MAX;
         
-        // ans is found
-        if(l1<=r2 && l2<=r1){
-            return max(l1,l2);
-        }
-        else if(l1>r2){
-            end=cut1-1; // search on the right side
+        if(n>m)
+        return kthElement(arr2, arr1, m, n, k);
+        
+        int low = max(0, k-m), high = min(k,n);
+        
+        while(low <= high){
             
+            int cut1 = low + (high - low)/2;
+            int cut2 = k  - cut1;
+            
+            int l1 = cut1 == 0?INT_MIN:arr1[cut1-1];
+            int l2 = cut2 == 0?INT_MIN:arr2[cut2-1];
+            int r1 = cut1 == n?INT_MAX:arr1[cut1];
+            int r2 = cut2 == m?INT_MAX:arr2[cut2];
+            
+            if(l1<=r2 && l2<=r1){
+                return max(l1,l2);
+            }else if(l1 > r2){
+                high = cut1 - 1;
+            }else{
+                low = cut1 + 1;
+            }
         }
-        else{
-            start=cut1+1 ; // seach on the left side;
-        }
-    }
-    return 1;
+        
+        return 1;
     }
 };
 
